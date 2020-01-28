e22b2b778ceb9b65e4cd1727bd0b4baacf3c920d
Sarthak Singhania <sathak2806singhania@gmail.com>
2020-01-28 17:39:03 +0530

    Update Page 4 Q2.py

--- Class 12/Practical questions/Page 4 Q2.py
+++ Class 12/Practical questions/Page 4 Q2.py
@@ -3,4 +3,14 @@ mysql=my.connect(host='localhost',user='root',password='root',database='school')
 cur=mysql.cursor()
 cur.execute('create table Sports(Stud_No int,Class int,Name char(50),Game char(50),Grade char(50))')
 stud=[x for x in range(10,16)]
-clas=
+class1=[7,8,7,7,9,10]
+name=['Sameer','Sujit','Kamal','Veena','Archana','Arpit']
+game=['Swimming','Skating','Football','Tennis','Cricket','Athletics']
+grade=['A','C','B','A','A','C']
+cur.execute('select count(Stud_No) from Sports where grade="A" and game="Cricket"')
+for i in cur:
+    print(i)
+cur.execute('alter table sports add column Address char(50)')
+cur.execute('select Game from sports where name like "A%"')
+for i in cur:
+    print(i)
