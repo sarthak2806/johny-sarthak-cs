998f7191f904a28065dc6649fe69899904f3ad02
Sarthak Singhania <sathak2806singhania@gmail.com>
2020-01-28 18:57:39 +0530

    Create Page 8 Q1.py

---
+++ Class 12/Practical questions/Page 8 Q1.py
@@ -0,0 +1,26 @@
+from random import randint
+l=[]
+def new_order():
+    global l
+    l.append(randint(100,999))
+def diqueue():
+    global l
+    return l.pop(0)
+def display():
+    global l
+    print(l)
+while True:
+    option=(int(input('\n1)Order a meal\n2)Order is ready\n3)Waiting Queue\n4)Exit\nEnter the option number:')))
+    if option==1:
+        enqueue()
+    elif option==2:
+        print(diqueue())
+        if len(l)==0:
+            print('Stack is underflow')
+    elif option==3:
+        if len(l)!=0:
+            display()
+        else:
+            print('Stack is underflow')
+    elif option==4:
+        break
