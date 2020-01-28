b009a89e1d884df739c2997aa676148dc4baf41d
Aryaman Agrawal <aryaman_agrawal@hotmail.com>
2020-01-28 19:19:30 +0530

    7

---
+++ Class 12/Practical questions/Page 7 Q1.py
@@ -0,0 +1,31 @@
+a=input("Enter elements separated by space: ").split()
+b=input("Enter elements separated by space: ").split()
+
+#Merge List
+L=a+b
+print(L)
+
+sum=0
+for x in a:
+    if x in b:
+       sum+=int(x)
+    else:
+        pass
+print (sum)
+
+c=a*2
+for x in range(len(a)): 
+    z=0
+    for y in range(x, x + len(a)): 
+        if b[z]==c[y]: 
+            z+=1
+        else: 
+            break
+    if z == len(a): 
+        print ('Yes')
+        break
+else:
+    print ('No')
+
+
+
