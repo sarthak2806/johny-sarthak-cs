04c3a529c2042189e3d09ce175a96510e6123b51
Sarthak Singhania <sathak2806singhania@gmail.com>
2020-01-28 18:01:31 +0530

    Update Page 6 Q1.py

--- Class 12/Practical questions/Page 6 Q1.py
+++ Class 12/Practical questions/Page 6 Q1.py
@@ -5,4 +5,7 @@ x2=[80,20,20,50,60]
 ax=plt.subplot()
 ax.bar(x1,y1,color='g',width=3)
 ax.bar(x2,y1,color='b',width=3)
+plt.title('Information')
+plt.xlabel('Days')
+plt.ylabel('Distance')
 plt.show()
