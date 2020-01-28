f0489f743fec86e4caa438e0c06616ba00bb27d2
Sarthak Singhania <sathak2806singhania@gmail.com>
2020-01-28 17:53:02 +0530

    Create Page 6 Q1.py

---
+++ Class 12/Practical questions/Page 6 Q1.py
@@ -0,0 +1,8 @@
+import matplotlib.pyplot as plt
+x1=[50,40,70,80,20]
+y1=[x for x in range(1,6)]
+x2=[80,20,20,50,60]
+ax=plt.subplot()
+ax.bar(x1,y1,color='g',width=3)
+ax.bar(x2,y1,color='b',width=3)
+plt.show()
